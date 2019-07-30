bf1ee5997b87f603765e5b76b16a28ccd92e45c5
HaehunLee <allman956@naver.com>
2019-07-30 10:23:54 +0900

    지식인, 검색 페이지 매핑

---
+++ link/src/main/java/kr/co/link/controller/SearchController.java
@@ -0,0 +1,15 @@
+package kr.co.link.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class SearchController {
+
+	
+	
+	@RequestMapping("/search.do")
+	public String search() {
+		return "search/searchMain";
+	}
+}
